e146be1a65297fd0843c4b04c394e5ee3869ad90
sahadan <sahadan@gmail.com>
2023-01-06 15:25:17 +0530

    added customer

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersComponent implements OnInit {
 
+  //declare variables
+  customerId: number=0;
+
   constructor() { }
 
   ngOnInit(): void {
