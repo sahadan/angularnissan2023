7508ec4df95a8e8dafba71e279c0b9631c1ac264
sahadan <sahadan@gmail.com>
2023-01-06 15:27:45 +0530

    added customername

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersComponent implements OnInit {
 
+  // declare variables
+  customerName: string ='';
+  
   constructor() { }
 
   ngOnInit(): void {
